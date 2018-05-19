853e405d46d5298e879f2dea0bb4487b87e080b1
Quang Anh <quanganh@aiti.com.vn>
2018-05-19 11:14:29 +0700

    update license

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -1,3 +1,31 @@
+/**
+ * Copyright 2018 xMobe https://www.xmobe.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+/**
+ * hexToBinary 
+ * Convert hex string to binary string, using for visually testing purpose 
+ * @param s
+ */
 export const hexToBinary = (s: string): string => {
     let ret: string = '';
     const lookupTable = {
@@ -20,10 +48,20 @@ export const hexToBinary = (s: string): string => {
     return ret;
 };
 
+/**
+ * byte2BinaryString 
+ * Convert byte to binary string, using for visually testing purpose
+ * 
+ * @param n 
+ */
 export const byte2BinaryString = (n): string => {
     return (('000000000' + n.toString(2)).substr(-8));
 }
 
+/**
+ * getCurrentTimestamp 
+ * Return the Local Current Time in second
+ */
 export const getCurrentTimestamp = (): number => {
     return ((new Date()).getTime() / 1000);
 }
--- test/core/wallet.spec.ts
+++ test/core/wallet.spec.ts
@@ -1,3 +1,26 @@
+/**
+ * Copyright 2018 xMobe https://www.xmobe.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
 import { expect } from 'chai';
 import { Chain } from '../../src/core';
 import { Wallet } from '../../src/wallet';
@@ -23,7 +46,7 @@ describe('BlockChain', () => {
         expect(w.getPrivateFromWallet()).to.equal(private_key);
     });
 
-    it("Should return Wallet amount 500", () => {
+    it("Should return Wallet amount", () => {
         Chain
             .getInstance()
             .then(instance => {
@@ -32,7 +55,7 @@ describe('BlockChain', () => {
 
                 let w = new Wallet();
                 let amount: number = w.getBalance(public_key, chain.getUnspentTxOuts());
-                if (public_key == '04995fe4c631d2de37e55c825c1f6cdcffd91106e103604552294b79b3418b72a49bb341602c8fc5699b76f6b68e75feca9188c55a2045776c490f188ea0dcfcb1') {
+                if (public_key == '04210b494b3ae21490abef3e88128e1fd04a1c9ff4aefe177e6f2a33369e6191eddd011004539589ba867bb7353e3d751b6110501d73ac20fd462149dfae914848') {
                     expect(amount).to.equal(500);
                 } else {
                     expect(amount).to.equal(0);
