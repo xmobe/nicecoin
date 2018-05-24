c408a8f90fba79cbb486dec083035c9a7e93337f
Quang Anh <quanganh@aiti.com.vn>
2018-05-24 14:41:36 +0700

    git rebase --skip# This is a combination of 3 commits.
    update wallet
    
    implement feature/core
    
    implement core

--- test/core/block.spec.ts
+++ test/core/block.spec.ts
@@ -2,16 +2,28 @@
  * Copyright 2018 xMobe https://www.xmobe.com
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
+<<<<<<< HEAD
  * of this software and associated documentation files (the `Software`), to deal
+=======
+ * of this software and associated documentation files (the "Software"), to deal
+>>>>>>> 853e405... update license
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
+<<<<<<< HEAD
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED `AS IS`, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+=======
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+>>>>>>> 853e405... update license
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -20,6 +32,11 @@
  * THE SOFTWARE.
  *
  */
+<<<<<<< HEAD
+=======
+
+import { expect } from 'chai';
+>>>>>>> 853e405... update license
 
 import { expect } from 'chai';
 import { Signale } from 'signale';
